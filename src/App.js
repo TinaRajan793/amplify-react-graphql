8b35e70b089e12f8348cd64b0645501d54e1662b
Rajan <t.rajan@accenture.com>
2023-07-19 18:49:53 +0100

    Added graphql and storage

--- src/App.js
+++ src/App.js
@@ -68,59 +68,58 @@ async function createNote(event) {
   }
 
   return (
-    <View className="App">
-      <Heading level={1}>My Notes App</Heading>
-      <View as="form" margin="3rem 0" onSubmit={createNote}>
-        <Flex direction="row" justifyContent="center">
-          <TextField
-            name="name"
-            placeholder="Note Name"
-            label="Note Name"
-            labelHidden
-            variation="quiet"
-            required
-          />
-          <TextField
-            name="description"
-            placeholder="Note Description"
-            label="Note Description"
-            labelHidden
-            variation="quiet"
-            required
-          />
-          <Button type="submit" variation="primary">
-            Create Note
-          </Button>
-        </Flex>
-      </View>
-      <Heading level={2}>Current Notes</Heading>
-      <View margin="3rem 0">
-        {notes.map((note) => (
-          <Flex
-            key={note.id || note.name}
-            direction="row"
-            justifyContent="center"
-            alignItems="center"
-          >
-            <Text as="strong" fontWeight={700}>
-              {note.name}
-            </Text>
-            <Text as="span">{note.description}</Text>
-            <Button variation="link" onClick={() => deleteNote(note)}>
-              Delete note
+   
+    <><View
+      name="image"
+      as="input"
+      type="file"
+      style={{ alignSelf: "end" }} /><View className="App">
+        <Heading level={1}>My Notes App</Heading>
+        <View as="form" margin="3rem 0" onSubmit={createNote}>
+          <Flex direction="row" justifyContent="center">
+            <TextField
+              name="name"
+              placeholder="Note Name"
+              label="Note Name"
+              labelHidden
+              variation="quiet"
+              required />
+            <TextField
+              name="description"
+              placeholder="Note Description"
+              label="Note Description"
+              labelHidden
+              variation="quiet"
+              required />
+            <Button type="submit" variation="primary">
+              Create Note
             </Button>
           </Flex>
-        ))}
-      </View>
-      <Button onClick={signOut}>Sign Out</Button>
-    </View>
-  <View
-    name="image"
-    as="input"
-    type="file"
-    style={{ alignSelf: "end" }}
-  />
+        </View>
+        <Heading level={2}>Current Notes</Heading>
+        <View margin="3rem 0">
+          {notes.map((note) => (
+            <Flex
+              key={note.id || note.name}
+              direction="row"
+              justifyContent="center"
+              alignItems="center"
+            >
+              <Text as="strong" fontWeight={700}>
+                {note.name}
+              </Text>
+              <Text as="span">{note.description}</Text>
+              <Button variation="link" onClick={() => deleteNote(note)}>
+                Delete note
+              </Button>
+            </Flex>
+          ))}
+        </View>
+        <Button onClick={signOut}>Sign Out</Button>
+      </View></>
+
   );
+
 };
 {notes.map((note) => (
   <Flex
